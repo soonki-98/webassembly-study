d691f6c2e484bdc9c79f304705efa67e6332ab46
soonki-98 <alstnsrl98@gmail.com>
2022-07-13 13:39:57 +0900

    추리게임 업그레이드

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,9 +6,8 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101); // 1 <= x < 101
-    println!("The Secret Number is {secret_number}");
+    let secret_number = rand::thread_rng().gen_range(1..=100); // 1 <= x < 101
+    let mut chance = 10;
 
     loop {
         println!("Please input your guess.");
@@ -17,22 +16,33 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        // let guess: u32 = guess.trim().parse().expect("please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
+        chance -= 1;
 
         let result = guess == secret_number;
 
         if result == true {
-            println!("You win!");
+            println!("You win!"); 
+            println!("Your score is {}", chance * 10);
             break;
         } else if guess < secret_number {
-            println!("Too small");
+            println!("Too small. chance = {chance}");
         } else {
-            println!("Too big!");
+            println!("Too big!. chance = {chance}");
         }
 
+        if chance == 0 {
+            println!("You failed!!!");
+            println!("answer is {secret_number}");
+            println!("Your score is {}", chance * 10);
+            break;
+        }
         // match guess.cmp(&secret_number) {
         //     Ordering::Less    => println!("Too small!"),
         //     Ordering::Greater => println!("Too big!"),
